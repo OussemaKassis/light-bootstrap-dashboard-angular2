3d1db5814f955f258fc2c69c8c4bdac9961bbc46
OussemaKassis <oussemakassis97@gmail.com>
2022-12-27 11:43:05 +0100

    [TASK] login + register

--- src/app/auth/login/login/login.component.ts
+++ src/app/auth/login/login/login.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  cred: any = {
+    email: "",
+    password: ""
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    localStorage.setItem('connected', 'false');
+    $('.message a').click(function(){
+      $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+   });
+  }
+
+  submit() {
+    console.log(this.cred);
+    localStorage.setItem('connected', 'true');
   }
 
 }
--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { sharedConsts } from 'app/shared/shared.consts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public login(credentials: any) {
+    return this.http.post(sharedConsts.API_ENDPOINT + '/user/getUser', credentials);
+  }
+
+  register(credentials: any) {
+    return this.http.post(sharedConsts.API_ENDPOINT + '/user/save', credentials);
+  }
 }
